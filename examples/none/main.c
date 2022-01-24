a81042e159b750bfe9a4fd8ca05068fab56e34f4
Jacob Walser <jwalser90@gmail.com>
2022-01-23 22:10:35 -0500

    so fast

--- examples/none/main.c
+++ examples/none/main.c
@@ -14,8 +14,8 @@ void main() {
 
 	// HSE is 24MHz
 	// set pll1 multiplication factor
-	uint32_t DIVM1 = 3; // division by 2
-	uint32_t DIVN1 = 99;
+	uint32_t DIVM1 = 1; // divide by 3 (8MHz)
+	uint32_t DIVN1 = 99; // multiply by 100 (800MHz)
 	uint32_t DIVP1 = 0; // no divisor
 	RCC->PLL1CFGR1 = (DIVM1<<16) | DIVN1;
 	RCC->PLL1CFGR2 = DIVP1;
@@ -30,9 +30,11 @@ void main() {
 	// enable pll1_p_ck output
 	RCC->PLL1CR |= RCC_PLL1CR_DIVPEN;
 
-	uint32_t DIVM2 = 1;
-	uint32_t DIVN2 = 43;
-	uint32_t DIVP2 = 1;
+
+	////// pll2 configuration
+	uint32_t DIVM2 = 1; // divide by 2 (12MHz)
+	uint32_t DIVN2 = 43; // multiply by 44 (528MHz)
+	uint32_t DIVP2 = 1; // divide by 2 (264MHz)
 	// set pll2 multiplication factor
 	RCC->PLL2CFGR1 = (DIVM2<<16) | DIVN2;
 	RCC->PLL2CFGR2 = DIVP2;
@@ -48,6 +50,24 @@ void main() {
 
 
 
+	//////// pll3 configuration
+	RCC->RCK3SELR |= 1; // select HSE as pll3 clock source
+	uint32_t DIVM3 = 1; // divide by 2 (12MHz)
+	uint32_t DIVN3 = 49; // multiply by 44 (528MHz)
+	RCC->PLL3CFGR1 = (DIVM3<<16) | DIVN3;
+	RCC->PLL3CFGR1 |= 1 << 24; // set IFRGE to x1
+
+	RCC->PLL3CFGR2 |= 1;
+	// set pll3 into integer mode
+	RCC->PLL3FRACR |= RCC_PLL3FRACR_FRACLE;
+
+	// turn pll3 on
+	RCC->PLL3CR |= RCC_PLL3CR_PLLON;
+	// wait for pll1 to be ready
+	while (!RCC->PLL3CR & RCC_PLL3CR_PLL3RDY);
+	// enable pll3_p_ck output
+	RCC->PLL3CR |= RCC_PLL3CR_DIVPEN;
+
 	// switch mpu clock to pll1_p_ck
 	uint32_t MPUSRC = 0x2;
 	RCC->MPCKSELR = MPUSRC;
@@ -56,17 +76,31 @@ void main() {
 	uint32_t AXISSRC = 0x2;
 	RCC->ASSCKSELR = AXISSRC;
 
+	RCC->APB1DIVR |= 1;
+	while (!RCC->APB1DIVR & RCC_APB1DIVR_APB1DIVRDY);
+	RCC->APB2DIVR |= 1;
+	while (!RCC->APB2DIVR & RCC_APB2DIVR_APB2DIVRDY);
+	RCC->APB3DIVR |= 1;
+	while (!RCC->APB3DIVR & RCC_APB3DIVR_APB3DIVRDY);
+	RCC->APB4DIVR |= 1;
+	while (!RCC->APB4DIVR & RCC_APB4DIVR_APB4DIVRDY);
+	RCC->APB5DIVR |= 2;
+	while (!RCC->APB5DIVR & RCC_APB5DIVR_APB5DIVRDY);
+	// switch mcu clock to pll3_p_ck
+	uint32_t MCUSSRC = 0x3;
+	RCC->MSSCKSELR = MCUSSRC;
+
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	int tmp = GPIOA->MODER;
 	tmp &= ~((0b10<<28)|(0b10<<22));
 	GPIOA->MODER = tmp;
 
-	int i;
-	while(1){
-		if (i++%0x10000 == 0) {
-			GPIOA->ODR = (~(GPIOA->ODR)) & ((1<<14) | (1<<11));
-		}
+	for(;;) {
+		for(int i = 0; i < 0x10000; i++);
+		GPIOA->ODR = (~(GPIOA->ODR)) & ((1<<14) | (1<<11));
+
 	}
+
 }
 
